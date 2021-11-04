9b3b112f20d67f478d465313fb812392f00b09ac
Tommy Ngo <tommy@sitehost.co.nz>
2021-11-05 11:51:09 +1300

    remove public

--- public/test.php
+++
@@ -1,8 +0,0 @@
-<html><head><title>It works!</title></head>
-<body>
-Your site is ready to go!<br />Host: <?php echo $_SERVER['HTTP_HOST']; ?>
-<?php
-echo "HERE";
-print_r(mail("tommy@sitehost.co.nz","LAMBDA TEST","Hello"));
-?>
-</body></html>
